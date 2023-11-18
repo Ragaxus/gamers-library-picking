e404d4bdbf5573d735d3c982b014cd1f9c32aa70
Stav Gold <ragaxus@gmail.com>
2023-11-18 15:23:30 -0600

    Add stuff to make router work

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,20 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        '^/api': {
-          target: 'http://localhost:8080/',
-          changeOrigin: true,
-          logLevel: "debug",
-        },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080/',
+        changeOrigin: true,
+        logLevel: "debug",
+      },
+    }
+  },
+  runtimeCompiler: true,
+  configureWebpack: {
+    devtool: "source-map",
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js'
       }
-    },
-    configureWebpack: {
-      devtool: "source-map"
     }
   }
+}
